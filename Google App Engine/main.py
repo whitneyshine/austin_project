5e749cd4410ca79fae6ccbc28b6a0ba9f23235b3
Christy Bell <75290129+christybell@users.noreply.github.com>
2021-06-11 00:03:14 -0500

    added Google App Engine folder and files

---
+++ Google App Engine/main.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    # Only run for local development.
+    app.run(host='127.0.0.1', port=8080, debug=True)
